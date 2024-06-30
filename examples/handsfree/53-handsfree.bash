b1ad4c7b84c69133b032b5ab4b01606073729220
borine <32966433+borine@users.noreply.github.com>
2024-06-30 16:15:49 +0100

    Improve systemd integration

--- examples/handsfree/53-handsfree.bash
+++ examples/handsfree/53-handsfree.bash
@@ -5,15 +5,20 @@
 [[ "${BLUEALSA_PCM_PROPERTY_CODEC,,}" =~ (cvsd|lc3-swb|msbc) ]] || exit
 [[ "$1" == "update" && "$BLUEALSA_PCM_PROPERTY_CHANGES" == *RUNNING* ]] || exit
 
-pidfile="/tmp/handsfree-${BLUEALSA_PCM_PROPERTY_ADDRESS}.pid"
+pidfile="$datadir/${BLUEALSA_PCM_PROPERTY_ADDRESS}.pid"
 
 case "$BLUEALSA_PCM_PROPERTY_RUNNING" in
 "true")
+	datadir="/tmp/bluealsa-agent/handsfree"
+	[[ -d "$datadir" ]] || mkdir -p datadir || {
+		echo "Cannot create state directory [$datadir]" >&2
+		exit 1
+	}
 	setsid --fork sh -c 'echo $$ >'"$pidfile"'; /usr/local/bin/handsfree.bash '"$BLUEALSA_PCM_PROPERTY_CODEC"' '"$BLUEALSA_PCM_PROPERTY_ADDRESS"'; rm '"$pidfile"
 	;;
 "false")
 	[[ -f "$pidfile" ]] || exit
-	pid="$(cat $pidfile)"
+	read pid <"$pidfile"
 	[[ "$(ps -eo ppid,comm | grep "$pid")" == *"$pid alsaloop" ]] && kill -- -"$pid"
 	rm "$pidfile"
 	;;
